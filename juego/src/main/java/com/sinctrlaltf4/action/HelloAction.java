22b663b94040051661476ca635f35396c476b3fe
Selene <spachecor03@gmail.com>
2024-09-18 23:40:13 +0200

    modificado todo a struts 2, funcionando, falta interfaz grafica

---
+++ juego/src/main/java/com/sinctrlaltf4/action/HelloAction.java
@@ -0,0 +1,24 @@
+package com.sinctrlaltf4.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
